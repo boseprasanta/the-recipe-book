f98871f0ec3df0fd905f01c661d636ffc6e20c65
Prasanta Bose <prasantabose1310@gmail.com>
2018-10-01 21:27:39 +0530

    Shopping list functions added

---
+++ src/models/ingredient.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+    /*
+        public name:string ;
+        public amount:number
+
+        constructor(name : string , amount : number){} --> To support this 
+        constructor(public name : string ,public amount : number){} --> Better Approach 
+    */
+   constructor(public name : string ,public amount : number){}
+}
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
@@ -18,8 +11,8 @@ export class ShoppingListPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
+  onAddItem(form:NgForm){
+    console.log(form);
   }
 
 }
